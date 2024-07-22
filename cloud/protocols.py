c2e2a1b2b0e5eaf5a0cba927550868ab5f779fbc
Leonardo Lima <leonardolima.aut@gmail.com>
2024-07-22 11:46:37 -0300

    feat: add methods to choose cloud url signed

--- cloud/protocols.py
+++ cloud/protocols.py
@@ -14,5 +14,16 @@ class StorageDownloader(Protocol):
 
 
 @runtime_checkable
+class StorageURLSigner(Protocol):
+    def generate_presigned_url(
+        self,
+        bucket_name: str,
+        source_filename: str,
+        expiration: int,
+        client_method_name: str,
+    ) -> str: ...
+
+
+@runtime_checkable
 class MessagePublisher(Protocol):
     def publish(self, recipient: str, message: str, **attrs: Any) -> None: ...
