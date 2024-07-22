e448a0dcb5f5a03a029146bafdee10d258263297
Leonardo Lima <leonardolima.aut@gmail.com>
2024-07-22 11:54:10 -0300

    feat: add s3 and storage tests

--- tests/google/test_storage.py
+++ tests/google/test_storage.py
@@ -1,8 +1,9 @@
+import datetime
 import unittest
 from unittest import mock
 
-from cloud.google.storage import Downloader, Uploader
-from cloud.protocols import StorageDownloader, StorageUploader
+from cloud.google.storage import Downloader, Uploader, URLSigner
+from cloud.protocols import StorageDownloader, StorageUploader, StorageURLSigner
 
 
 class TestStorageUploader(unittest.TestCase):
@@ -51,3 +52,36 @@ class TestStorageDownloader(unittest.TestCase):
 
         blob = bucket.blob.return_value
         blob.download_to_filename.assert_called_once_with(destination)
+
+
+class TestStorageURLSigner(unittest.TestCase):
+    @mock.patch("google.cloud.storage.Client")
+    @mock.patch("google.auth.default")
+    def test_storage_urlsigner(self, default_mock, client_mock):
+        default_mock.return_value = (mock.MagicMock(token=None), None)
+        urlsigner = URLSigner()
+
+        self.assertIsInstance(urlsigner, StorageURLSigner)
+        client_mock.assert_called_once_with()
+
+        bucket_name = "test-bucket"
+        object_name = "test.txt"
+        expiration = 60
+        client_method_name = "GET"
+
+        urlsigner.generate_presigned_url(bucket_name, object_name)
+
+        cli = client_mock.return_value
+        cli.bucket.assert_called_once_with(bucket_name)
+
+        bucket = cli.bucket.return_value
+        bucket.blob.assert_called_once_with(object_name)
+
+        blob = bucket.blob.return_value
+        blob.generate_signed_url.assert_called_once_with(
+            version="v4",
+            service_account_email=mock.ANY,
+            access_token=None,
+            expiration=datetime.timedelta(minutes=expiration),
+            method=client_method_name,
+        )
