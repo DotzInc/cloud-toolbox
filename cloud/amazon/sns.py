80f8780cda8795ec4147e92a93eb6b1b91115308
Juliano Fernandes <julianofernandes@gmail.com>
2024-08-26 15:00:50 -0300

    feat: return the id of the published message
    - Updated the Publisher protocol to return a value of type string from the publish method.
    - Updated the Publisher protocol implementations for Amazon SNS, SQS, and Google PubSub services.
    - Updated unit tests for the Publisher protocol implementations.

--- cloud/amazon/sns.py
+++ cloud/amazon/sns.py
@@ -7,5 +7,6 @@ class Publisher:
     def __init__(self, *args: Any, **kwargs: Any):
         self.client = boto3.client("sns", *args, **kwargs)
 
-    def publish(self, recipient: str, message: str, **attrs: Any) -> None:
-        self.client.publish(TargetArn=recipient, Message=message, **attrs)
+    def publish(self, recipient: str, message: str, **attrs: Any) -> str:
+        response = self.client.publish(TargetArn=recipient, Message=message, **attrs)
+        return response["MessageId"]
--- tests/amazon/test_sns.py
+++ tests/amazon/test_sns.py
@@ -8,6 +8,10 @@ from cloud.protocols import MessagePublisher
 class TestMessagePublisher(unittest.TestCase):
     @mock.patch("boto3.client")
     def test_message_publisher(self, client_mock):
+        response = {"MessageId": "4c6e445c-308e-46d5-90f8-aa77d29298d4"}
+        cli = client_mock.return_value
+        cli.publish.return_value = response
+
         publisher = Publisher()
 
         self.assertIsInstance(publisher, MessagePublisher)
@@ -16,7 +20,7 @@ class TestMessagePublisher(unittest.TestCase):
         topic = "arn:aws:sns:us-east-1:123456789012:test-topic"
         message = "test message"
 
-        publisher.publish(topic, message)
+        message_id = publisher.publish(topic, message)
 
-        cli = client_mock.return_value
+        self.assertEqual(message_id, response["MessageId"])
         cli.publish.assert_called_once_with(TargetArn=topic, Message=message)
