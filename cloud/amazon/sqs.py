7bdfa26b8bfb8ed654830a344b2c86b965fa0b5b
Juliano Fernandes <julianofernandes@gmail.com>
2024-09-05 12:29:14 -0300

    feat: add message ordering support for aws sns, sqs, and google pub/sub.
    This commit introduces enhancements to the `publish` method in the AWS SNS and SQS publishers as well as the Google Pub/Sub publisher classes to allow optional message ordering. For AWS, it adds parameters for `MessageGroupId` and `MessageDeduplicationId`, utilizing `uuid` for unique deduplication IDs. In Google Pub/Sub, a new `OrderedPublisher` class is introduced that enables message ordering.
    
    Additionally, test cases have been updated and expanded to ensure the new features are covered, including tests for attribute handling and ordering.

--- cloud/amazon/sqs.py
+++ cloud/amazon/sqs.py
@@ -1,3 +1,4 @@
+import uuid
 from typing import Any
 
 import boto3
@@ -7,6 +8,15 @@ class Publisher:
     def __init__(self, *args: Any, **kwargs: Any):
         self.client = boto3.client("sqs", *args, **kwargs)
 
-    def publish(self, recipient: str, message: str, **attrs: Any) -> str:
-        response = self.client.send_message(QueueUrl=recipient, MessageBody=message, **attrs)
+    def publish(self, recipient: str, message: str, group: str = "", **attrs: Any) -> str:
+        kwargs = {}
+
+        if group:
+            kwargs["MessageGroupId"] = group
+            kwargs["MessageDeduplicationId"] = str(uuid.uuid4())
+
+        if attrs:
+            kwargs["MessageAttributes"] = attrs
+
+        response = self.client.send_message(QueueUrl=recipient, MessageBody=message, **kwargs)
         return response["MessageId"]
